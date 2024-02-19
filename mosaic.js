ed25b089a1a7f6930a2b008e1ca7ac291091d2ee
ashrv12 <151522619+ashrv12@users.noreply.github.com>
2024-02-19 19:17:52 +0800

    updater

--- mosaic.js
+++ mosaic.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
-app.get("/asim", (req, res) => {
-  const tiel = 999111100003333;
+app.get("/read", (req, res) => {
+  const data = fs.readFileSync("articles.json", "utf8");
+
+  res.json(JSON.parse(data));
+});
+
+app.get("/read/create", (req, res) => {
+  const { title, desc } = req.query;
+
+  const data = fs.readFileSync("articles.json", "utf8");
+  const list = JSON.parse(data);
+
+  list.push({
+    title: title,
+    desc: desc,
+  });
+
+  fs.writeFileSync("articles.json", JSON.stringify(list));
   res.json([
     {
-      title: tiel,
-      descriptic: "Zero Tolerancy",
+      thewrite: "was successful",
     },
   ]);
 });
