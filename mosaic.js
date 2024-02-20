40804ab609b8ccb3e9000e758652ce95dcecc646
ashrv12 <151522619+ashrv12@users.noreply.github.com>
2024-02-20 18:14:56 +0800

    tragic

--- mosaic.js
+++ mosaic.js
@@ -3,19 +3,36 @@ const fs = require("fs");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+app.post("/blast", (req, res) => {
+  const body = req.body;
+
+  console.log({ body });
+
+  res.json([
+    {
+      status: "Success",
+    },
+  ]);
+});
+
 app.get("/read", (req, res) => {
   const data = fs.readFileSync("articles.json", "utf8");
 
   res.json(JSON.parse(data));
 });
 
-app.get("/read/create", (req, res) => {
-  const { title, desc } = req.query;
+app.post("/read/create", (req, res) => {
+  const { title, desc } = req.body;
 
   const data = fs.readFileSync("articles.json", "utf8");
   const list = JSON.parse(data);
 
+  const articleId = list.length + 1;
+
   list.push({
+    id: articleId,
     title: title,
     desc: desc,
   });
@@ -27,6 +44,31 @@ app.get("/read/create", (req, res) => {
     },
   ]);
 });
+
+app.put("/update/:id", (req, res) => {
+  const id = req.params.id;
+
+  console.log(id);
+
+  const updateData = req.body;
+
+  res.json([{ write: "Success" }]);
+});
+
+// app.get("/read/delete", (req, res) => {
+//   const data = fs.readFileSync("articles.json", "utf8");
+//   const list = JSON.parse(data);
+
+//   list.pop();
+//   res.json(
+//     [
+//       {
+//         thewrite: "popped last",
+//       },
+//     ],
+//     JSON.parse(data)
+//   );
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
